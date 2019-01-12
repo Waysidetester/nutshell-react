3a301d42e8483b1297550ed6aed9e326ab6946f6
WaysideTester <dylanmurray211@outlook.com>
2019-01-12 13:44:26 -0600

    changing article to input on edit select

--- src/components/pages/articles/article/article.js
+++ src/components/pages/articles/article/article.js
@@ -5,16 +5,83 @@ import 'firebase/auth';
 import './article.scss';
 
 class Article extends React.Component {
+  state = {
+    article: {
+      title: this.props.articleTitle,
+      synopsis: this.props.articleSynopsis,
+      url: this.props.articleUrl,
+      uid: this.props.articleUid,
+    },
+    editing: false,
+  }
+
+  startEditing = () => {
+    this.setState({ editing: true });
+  }
+
   render() {
+    const editFields = (input, e) => {
+      e.preventDefault();
+      const tempArticle = { ...this.state.newListing };
+      tempArticle[input] = e.target.value;
+      this.setState({ article: tempArticle });
+    };
+
+    // get UID
     const currentUid = firebase.auth().currentUser.uid;
-    if (this.props.articleUid === currentUid) {
+
+    // checks if UID matched article UID
+    if (this.state.article.uid === currentUid) {
+
+      // checks if user wants to edit their article
+      if (this.state.editing) {
+        // builds article in editing state
+        return (
+          <div className='card'>
+          <div className='card-body'>
+          
+          {/* title input feild */}
+          <div class='input-group mb-3'>
+            <input className='form-control' aria-label='Default' aria-describedby='inputGroup-sizing-default' value={this.state.article.title}/>
+            <div class='input-group-prepend'>
+              <span class='input-group-text' id='inputGroup-sizing-default'>Title</span>
+            </div>
+          </div>
+          {/* synopsis input feild */}
+          <div class='input-group mb-3'>
+            <input className='form-control' aria-label='Default' aria-describedby='inputGroup-sizing-default' value={this.state.article.synopsis}/>
+            <div class='input-group-prepend'>
+              <span class='input-group-text' id='inputGroup-sizing-default'>Synopsis</span>
+            </div>
+          </div>
+          {/* url input feild */}
+          <div class='input-group mb-3'>
+            <input className='form-control' aria-label='Default' aria-describedby='inputGroup-sizing-default' value={this.state.article.url}/>
+            <div class='input-group-prepend'>
+              <span class='input-group-text' id='inputGroup-sizing-default'>URL</span>
+            </div>
+          </div>
+            <div>
+              <button className='btn btn-primary'>Update</button>
+            </div>
+          </div>
+        </div>
+        );
+      }
+
+      // builds article with alteration buttons
       return (
         <div className='card'>
           <div className='card-body'>
-            <h5 className='card-title'>{this.props.articleTitle}</h5>
-            <p className='card-text'>{this.props.articleSynopsis}</p>
-            <a href={this.props.articleUrl} className='card-link'>{this.props.articleUrl}</a>
+            <h5 className='card-title'>{this.state.article.title}</h5>
+            <p className='card-text'>{this.state.article.synopsis}</p>
+            <a href={this.state.article.url} className='card-link'>{this.state.article.url}</a>
             <div>
+              {/* places article in editing state */}
+              <button
+              className='btn btn-warning'
+              onClick={this.startEditing}>Edit</button>
+              {/* deletes article from database */}
               <button
               className='btn btn-danger'
               onClick={
@@ -34,6 +101,7 @@ class Article extends React.Component {
       );
     }
 
+    // builds default article
     return (
         <div className='card'>
           <div className='card-body'>
